a0d46a18f46edbf66175c0db6ec8f736beb90db4
Akshat Tyagi <akshat.tyagi1909@gmail.com>
2026-06-14 21:48:05 +0530

    Update calculator UI and functionality

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const additionBtn = document.querySelector(".addition-btn");
 const digitBtns = document.querySelectorAll(".digit-btn");
 const operatorBtns = document.querySelectorAll(".operator-btn");
 const equalBtn = document.querySelector(".equal-btn");
+const backspaceBtn = document.querySelector(".backspace-btn");
 
 let result = 0;
 
@@ -14,6 +15,9 @@ let opArr = ["+", "-", "×", "÷"];
 digitBtns.forEach((digitBtn) => {
   digitBtn.addEventListener("click", () => {
     let num1 = digitBtn.textContent;
+    if (display.textContent === "0") {
+      display.textContent = "";
+    }
     display.textContent += num1;
     num1 = Number(num1);
   });
@@ -21,8 +25,14 @@ digitBtns.forEach((digitBtn) => {
 
 operatorBtns.forEach((operatorBtn) => {
   operatorBtn.addEventListener("click", () => {
-    let operator = operatorBtn.textContent;
     let str = display.textContent;
+    if (opArr.includes(str[str.length - 1])) {
+      return;
+    }
+    let operator = operatorBtn.textContent;
+    if (display.textContent === "0" && operator === "-") {
+      return (display.textContent = operator);
+    }
     doCalculation(str);
     display.textContent += operator;
   });
@@ -34,9 +44,6 @@ function doCalculation(str) {
     let opIndex = str.indexOf(op);
     let num1 = Number(str.slice(0, opIndex));
     let num2 = Number(str.slice(opIndex + 1, str.length));
-    console.log(num1);
-    console.log(num2);
-    console.log(op);
     if (op === "+") {
       result = addition(num1, num2);
     } else if (op === "-") {
@@ -51,13 +58,20 @@ function doCalculation(str) {
 }
 
 equalBtn.addEventListener("click", () => {
-    let str = display.textContent;
-    doCalculation(str);
+  let str = display.textContent;
+  doCalculation(str);
 });
 
 clearBtn.addEventListener("click", () => {
-    display.textContent = "";
-})
+  display.textContent = "0";
+});
+
+backspaceBtn.addEventListener("click", () => {
+  if (display.textContent.length === 1 || display.textContent.length === 0) {
+    return (display.textContent = "0");
+  }
+  display.textContent = display.textContent.slice(0, -1);
+});
 
 function addition(num1, num2) {
   return num1 + num2;
